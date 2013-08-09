6bf92137be749663cba2be16e9ed3f9798c55e2c
Matt H <mathphreak@gmail.com>
2013-08-09 10:28:58 -0600

    fixed meteor.sh to quit requiring extra fixing
    meteor.sh now sets up the mount point for .meteor that fixes everything

--- meteor.sh
+++ meteor.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo apt-get install -y curl
-curl https://install.meteor.com | sudo sh
+if [ ! -d /home/vagrant/homework ]; then
+    sudo apt-get install -y curl
+    curl https://install.meteor.com | sudo sh
+    cd /home/vagrant
+    su vagrant -c 'meteor create homework'
+    su vagrant -c 'meteor add coffeescript less bootstrap backbone'
+    su vagrant -c 'meteor remove autopublish'
+    cd /vagrant/homework
+    rm -vrf .meteor
+    mkdir .meteor
+    echo "sudo mount --bind /home/vagrant/homework/.meteor/ /vagrant/homework/.meteor/" >> /home/vagrant/.bashrc
+fi
